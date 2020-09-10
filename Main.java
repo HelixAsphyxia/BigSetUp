876c2854832db557cc88c8294aa73476eb23a821
Carlos Valtierra <18240279@leon.tecnm.mx>
2020-09-10 06:31:40 -0700

    Programa de Vigenere

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vigenere;
+public class Main{
+/**
+ *
+ * @author valti
+ */
+public static void main(String[] args){
+Vigenere c;
+c=new Vigenere("holamundu","contra");
+}
+    
+}
